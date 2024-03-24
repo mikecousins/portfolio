9979a77ac184eb6b0690f85e4a377bbb94882134
Mike Cousins <mike@mikecousins.com>
2024-03-24 09:37:03 -0600

    Getting price dynamically

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import {
   json,
+  redirect,
   type LoaderFunctionArgs,
   type MetaFunction,
 } from "@remix-run/node";
@@ -9,7 +10,10 @@ import { userPrefs } from "~/cookie.server";
 export const meta: MetaFunction = () => {
   return [
     { title: "XEQT Portfolio Value" },
-    { name: "description", content: "Calculate the value of your XEQT portfolio" },
+    {
+      name: "description",
+      content: "Calculate the value of your XEQT portfolio",
+    },
   ];
 };
 
@@ -17,14 +21,25 @@ export async function loader({ request }: LoaderFunctionArgs) {
   const cookieHeader = request.headers.get("Cookie");
   const cookie = (await userPrefs.parse(cookieHeader)) || {};
 
-  return json({ portfolio: cookie.shares * 30.13 });
+  if (!cookie.shares) return redirect("/settings");
+
+  const response = await fetch(
+    "https://www.blackrock.com/ca/investors/en/products/309480/ishares-core-equity-etf-portfolio"
+  );
+  const body = await response.text();
+  const spanLocation = body.indexOf('<span class="header-nav-data">');
+  const priceElement = body.substring(spanLocation, spanLocation + 50);
+  const rawPrice = priceElement.substring(35, 40);
+  const price = Number(rawPrice);
+
+  return json({ portfolio: cookie.shares * price });
 }
 
 export default function Index() {
   const { portfolio } = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>${portfolio ?? 'NaN'}</h1>
+      <h1>${portfolio ?? "NaN"}</h1>
       <Link to="/settings">Settings</Link>
     </div>
   );
